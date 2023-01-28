6f4b0be70a05dcbedb539b7474cc7860d04fc254
Walker Le <walker.le88@gmail.com>
2023-01-28 15:45:45 -0700

    commit tree 3b4511682a2ebe19b8ab9a093451305df3f07739

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+# greet_programmer
+
+def greet name
+  puts "Hello, #{name}!"
+end
+
+# greet "Naureen"
+
+def greet_with_default name = "programmer"
+  puts "Hello, #{name}!"
+end
+
+def add num1, num2
+  num1 + num2
+end
+
+# puts add 1, 2
+
+def halve number
+  if number.class != Integer
+    return nil
+  end
+  number / 2
+end
+
+# puts 8.class
+puts halve 8
+puts halve "not a number"
